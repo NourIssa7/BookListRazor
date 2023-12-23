a3f97f3d0b352eb6406b13b7bbd63328571425f4
NourIssa7 <nouracm7@gmail.com>
2023-12-23 11:57:26 -0500

    Add project files.

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext //class inside Microsoft.EntityFrameworkCore;
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        //add a model to the db inside the db context we need an entry then add the db context inside startup
+        public DbSet<Book> Book { get; set;}
+    }
+}
---
+++ BookListRazor/Program.cs
@@ -0,0 +1,29 @@
+using BookListRazor.Model;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
