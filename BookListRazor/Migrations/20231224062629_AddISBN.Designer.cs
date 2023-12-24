0ae63f6304874a12cb440b4df913000f8bc13550
NourIssa7 <nouracm7@gmail.com>
2023-12-24 04:48:53 -0500

    Create New Book

--- BookListRazor/Migrations/20231224062629_AddISBN.Designer.cs
+++ BookListRazor/Migrations/20231224062629_AddISBN.Designer.cs
@@ -3,6 +3,7 @@ using BookListRazor.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookListRazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231224062629_AddISBN")]
+    partial class AddISBN
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -9,5 +9,7 @@ namespace BookListRazor.Model
         [Required]
         public string Name { get; set; }
         public string Author {  get; set; }
+
+        public string ISBN {  get; set; }
     }
 }
